64606090d3d8420f8afddedcd5fd12d26f2da235
sanjarqurbiyozov <sanjarqurbiyozov002@gmail.com>
2026-04-15 18:23:47 +0500

    I completed practical exercise 4.

--- practice.py
+++ practice.py
@@ -1,21 +1,30 @@
-cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
-for car in cars:
-  if car == 'gm':
-    print(car.upper())
-  else:
-    print(car.title())
+# cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+# for car in cars:
+#   if car == 'gm':
+#     print(car.upper())
+#   else:
+#     print(car.title())
 
 
-cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
-for car in cars:
-  if car != 'gm':
-    print(car.title())
-  else:
-    print(car.upper())
+# cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+# for car in cars:
+#   if car != 'gm':
+#     print(car.title())
+#   else:
+#     print(car.upper())
 
 
-name = input("Ismingizni kiritish>>>")
-if name.lower() == 'admin':
-  print(f"Xush kelibsiz, {name.title()}. Foydalanuvchilar ro'yxatini ko'rasizmi")
+# name = input("Ismingizni kiritish>>>")
+# if name.lower() == 'admin':
+#   print(f"Xush kelibsiz, {name.title()}. Foydalanuvchilar ro'yxatini ko'rasizmi")
+# else:
+#   print(f"Xush kelibsiz, {name.title()}")
+
+
+a = int(input("1-soni kiriting:>>> "))
+b = int(input("2-soni kiriting:>>> "))
+
+if a == b:
+  print("Sonlar teng!!")
 else:
-  print(f"Xush kelibsiz, {name.title()}")
+  print("Sonlar bir biridan farq qiladi")
