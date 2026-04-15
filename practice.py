988f0b0996c317542d29f457aeafa02befad178e
sanjarqurbiyozov <sanjarqurbiyozov002@gmail.com>
2026-04-15 18:31:28 +0500

    I completed practical exercise 5.

--- practice.py
+++ practice.py
@@ -1,30 +1,42 @@
-# cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
-# for car in cars:
-#   if car == 'gm':
-#     print(car.upper())
-#   else:
-#     print(car.title())
+# # 1-mashq
+# # cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+# # for car in cars:
+# #   if car == 'gm':
+# #     print(car.upper())
+# #   else:
+# #     print(car.title())
+
+# # 2-mashq
+# # cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+# # for car in cars:
+# #   if car != 'gm':
+# #     print(car.title())
+# #   else:
+# #     print(car.upper())
+
+# # 3-mashq
+# # name = input("Ismingizni kiritish>>>")
+# # if name.lower() == 'admin':
+# #   print(f"Xush kelibsiz, {name.title()}. Foydalanuvchilar ro'yxatini ko'rasizmi")
+# # else:
+# #   print(f"Xush kelibsiz, {name.title()}")
+
+# # 4-mashq
+# a = int(input("1-soni kiriting:>>> "))
+# b = int(input("2-soni kiriting:>>> "))
 
 
-# cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
-# for car in cars:
-#   if car != 'gm':
-#     print(car.title())
-#   else:
-#     print(car.upper())
-
-
-# name = input("Ismingizni kiritish>>>")
-# if name.lower() == 'admin':
-#   print(f"Xush kelibsiz, {name.title()}. Foydalanuvchilar ro'yxatini ko'rasizmi")
+# if a == b:
+#   print("Sonlar teng!!")
 # else:
-#   print(f"Xush kelibsiz, {name.title()}")
+#   print("Sonlar bir biridan farq qiladi")
 
+# # 5-mashq
+# a = float(input("Soni kiriting:>>> "))
 
-a = int(input("1-soni kiriting:>>> "))
-b = int(input("2-soni kiriting:>>> "))
-
-if a == b:
-  print("Sonlar teng!!")
-else:
-  print("Sonlar bir biridan farq qiladi")
+# if a > 0:
+#   print("Musbat son!")
+# elif a < 0:
+#   print("Manfiy son!")
+# else:
+#   print("Siz 0 sonini kiritdingiz!")
