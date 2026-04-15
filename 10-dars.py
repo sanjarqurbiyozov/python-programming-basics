0d4370b2bf28c3ba41cc5b3d37222f380c1399b7
sanjarqurbiyozov <sanjarqurbiyozov002@gmail.com>
2026-04-15 17:51:14 +0500

    Foydanaluvchining ismi ali yoki emasligini tekshrish darturi yozish

--- 10-dars.py
+++ 10-dars.py
@@ -5,3 +5,12 @@ for avto in avtolar:              # avtolar ichidagi har bir avto uchun ...
     print(avto.upper())           # avto nomi hamma harflari katta bilan yoz.
   else:   # aks holda ...
     print(avto.title())   # avto nomini birinchi harfini katta harfda yoz.
+
+
+
+# Qiymatlarning teng emasligini tekshrish. Checking for inequality of values.
+name = input('Ismingiz nima? \n>>>') # Foydalanuvchidan ismini soraymiz.
+if name.lower() != 'ali':            # Agar ism ali ga teng bolmasa ...
+  print(f"Uzr, {name.title()} biz Alini kutyabmiz.") # quyidagi habar chiqadi
+else:
+  print("Salom, Ali!")
