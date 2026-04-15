0b44b3d284aad0a6d914e634986d9b28561c1e91
sanjarqurbiyozov <sanjarqurbiyozov002@gmail.com>
2026-04-15 18:11:13 +0500

    I completed practical exercise 2.

--- practice.py
+++ practice.py
@@ -4,3 +4,11 @@ for car in cars:
     print(car.upper())
   else:
     print(car.title())
+
+
+cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+for car in cars:
+  if car != 'gm':
+    print(car.title())
+  else:
+    print(car.upper())
